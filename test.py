63d8ad4835d7b61e0435567586ec6eca68b41e39
root <root@slave1.hadoop>
2017-03-16 13:51:13 +0800

    接口的简单封装

---
+++ test.py
@@ -0,0 +1,11 @@
+from lib.ansibleapi.playbook import PlayBookJob
+
+if __name__ == '__main__':
+    PlayBookJob(playbooks=['/tmp/test.yml'],
+                host_list=['172.16.10.54', '172.16.10.53'],
+                remote_user='root',
+                group_name="test",
+                forks=20,
+                ext_vars=None,
+                passwords='123456'
+                )
